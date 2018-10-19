7d5c25db04c1ba5d6d5e711c3ac80fcf0da18d09
D14na <support@d14na.org>
2018-10-18 21:18:41 -0400

    Added integration of Vue.js and ZeroApi.

---
+++ js/main.js
@@ -0,0 +1,63 @@
+class ZeroApp extends ZeroApi {
+    setSiteInfo(_siteInfo) {
+        /* Set Zer0net summary details. */
+        App.ziteAddress = _siteInfo.address
+        App.zitePeers = _siteInfo.peers
+        App.ziteSize = _siteInfo.settings.size
+    }
+
+    onOpen() {
+        /* Call super. */
+        super.onOpen()
+
+        this.cmd('siteInfo', [], function (_siteInfo) {
+            Zero.setSiteInfo(_siteInfo)
+        })
+    }
+
+    onEvent(_event, _message) {
+        if (_event === 'setSiteInfo')
+            this.setSiteInfo(_message.params)
+        else
+            this.log('Unknown event:', _event)
+    }
+}
+
+/**
+ * Vue Application Manager (Configuration)
+ */
+const vueAppManager = {
+    el: '#app',
+    data: () => ({
+        /* ZeroApp / ZeroApi Manager */
+        zero: null,
+
+        /* App Summary */
+        appTitle: 'Hell0',
+        appDesc: 'Boilerplate templates for Zer0net publishers.',
+
+        /* Zite Summary */
+        ziteAddress: 'n/a',
+        zitePeers: 0,
+        ziteSize: 0
+    }),
+    mounted: function () {
+        /* Initialize application. */
+        this._init()
+    },
+    computed: {
+        // TODO
+    },
+    methods: {
+        _init: function () {
+            /* Initialize new Zer0net app manager. */
+            // NOTE Globally accessible (e.g. Zero.cmd(...))
+            window.Zero = new ZeroApp()
+
+            console.info('Vue + Zer0net have loaded successfully!')
+        }
+    }
+}
+
+/* Initialize the Vue app manager. */
+const App = new Vue(vueAppManager)
